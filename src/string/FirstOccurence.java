c4355c2271e79ea74a798f1460a4d012331b5f44
prathamawasthi84 <pratham.awasthi11@gmail.com>
2025-08-08 21:38:56 +0530

    leetcode sol

--- src/string/FirstOccurence.java
+++ src/string/FirstOccurence.java
@@ -1,4 +1,18 @@
 package string;
 
 public class FirstOccurence {
+    public static int strStr(String haystack, String needle){
+        if (haystack.equals(haystack.toLowerCase()) && needle.equals(needle.toLowerCase())){
+        if(haystack.contains(needle)){
+            int index = haystack.indexOf(needle);
+            return index;
+        }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+     String hay = "dad";
+     String ned = "sad";
+        System.out.println(strStr(hay,ned));
+    }
 }

c0c80a4c3e368183c34700cff0f820644b69555b
prathamawasthi84 <pratham.awasthi11@gmail.com>
2025-08-08 22:26:32 +0530

    leetcode sol

--- src/string/LengthOfLastWord.java
+++ src/string/LengthOfLastWord.java
@@ -1,4 +1,13 @@
 package string;
 
 public class LengthOfLastWord {
+    public static int lengthOfLastWord(String s) {
+    String word = s.trim().replaceAll(".*\\s+","");
+    int len = word.length();
+    return len;
+    }
+    public static void main(String[] args) {
+     String w = "King   of    Priates  ";
+        System.out.println(lengthOfLastWord(w));
+    }
 }

976ef4ae69f1b7fd0170673f51b3e3dd3ff73a20
prathamawasthi84 <pratham.awasthi11@gmail.com>
2025-08-12 23:17:31 +0530

    leetcode sol

---
+++ src/string/Base7.java
@@ -0,0 +1,20 @@
+package string;
+
+public class Base7 {
+    public String convertToBase7(int num) {
+        if(num == 0){
+            return "0";
+        }
+        boolean isNeg = num<0;
+        num = Math.abs(num);
+        StringBuilder sb = new StringBuilder();
+        while(num>0){
+            sb.append(num%7);
+            num = num/7;
+        }
+        if(isNeg)
+            sb.append("-");
+
+        return sb.reverse().toString();
+    }
+}
